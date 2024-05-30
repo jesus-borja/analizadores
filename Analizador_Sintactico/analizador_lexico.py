0a136cf5164809e71449b0c063132767778a3747
Angel Rodrigo <arbyking102@gmail.com>
2024-05-29 19:12:29 -0600

    Analizador sintactico

---
+++ Analizador_Sintactico/analizador_lexico.py
@@ -0,0 +1,232 @@
+# ----------------- Librerias Usadas ----------------- #
+import ply.lex as lex
+
+# ---------------------- TOKENS ---------------------- #
+
+# Lista para almacenar los tokens no reconocidos
+errores = []
+
+# Palabras reservadas
+keywords = [
+    "FUNCTION",
+    "ARROW", # nombre para "->"
+    "IF",
+    "ELSE",
+    "FOR",
+    "IN",
+    "WHILE",
+    "RETURN",
+]
+
+# Tipos de datos y literales
+datatypes = [
+    # Tipos de datos
+    "TYPE_INTEGER",
+    "TYPE_STRING",
+    "TYPE_FLOAT",
+    "TYPE_DOUBLE",
+    "TYPE_BOOL",
+    "TYPE_VOID",
+    "TYPE_NULL",
+
+    # Literales de datos
+    "STRING",
+    "INT",
+    "FLOAT",
+    "DOUBLE",
+    "BOOL",
+]
+
+# Tabla de tokens
+tokens = [
+    "IDENTIFIER", # Nombres de variables o funciones
+
+    # Operadores matemáticos
+    "PLUS",
+    "MINUS",
+    "TIMES",
+    "DIVIDE",
+    "MODULO",
+    "POWER",
+
+    # Operadores lógicos
+    # "EQUAL", # no se usa creo
+    "ASSIGN_OP",
+    "COMPARISON",
+    "LESS_THAN",
+    "GREATER_THAN",
+    "GREATER_EQUAL",
+    "LESS_EQUAL",
+    "NOT_EQUAL",
+    "NOT",
+    "AND",
+    "OR",
+    
+    # Comentarios
+    "COMMENT_START",
+    "COMMENT_END",
+
+    # Caracteres
+    "LEFT_PAREN",
+    "RIGHT_PAREN",
+    "LEFT_BRACKET",
+    "RIGHT_BRACKET",
+    "LEFT_BRACE",
+    "RIGHT_BRACE",
+    "SEMICOLON",
+    "COMMA",
+    "DOT",
+    "COLON",
+
+    # Otros
+    "SPACE",
+    "NEWLINE",
+]
+
+# Todos los tokens aceptados por el lexer
+tokens = tokens + keywords + datatypes
+
+# ---------------------- Expresiones Regulares "simples" ---------------------- #
+
+# Operadores matemáticos
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_MODULO = r'%'
+
+# Operadores de asignación y comparación
+t_ASSIGN_OP = r':='
+t_COLON = r':'
+t_SEMICOLON = r';'
+t_COMMA = r','
+t_DOT = r'\.'
+t_COMPARISON = r'='
+
+# Operadores de comparación
+t_LESS_THAN = r'<'
+t_GREATER_THAN = r'>'
+t_GREATER_EQUAL = r'>='
+t_LESS_EQUAL = r'<='
+t_NOT_EQUAL = r'!='
+
+# Operadores lógicos
+t_NOT = r'!'
+t_AND = r'&&'
+t_OR = r'\|\|'
+
+# Paréntesis, corchetes y llaves
+t_LEFT_PAREN = r'\('
+t_RIGHT_PAREN = r'\)'
+t_LEFT_BRACKET = r'\['
+t_RIGHT_BRACKET = r'\]'
+t_LEFT_BRACE = r'\{'
+t_RIGHT_BRACE = r'\}'
+
+# ----- Expresiones Regulares para tokens con mayor funcionalidad ----- #
+
+# Tipos de datos
+def t_TYPE_INTEGER(t):
+    r'int'
+    return t
+
+def t_TYPE_STRING(t):
+    r'str'
+    return t
+def t_TYPE_FLOAT(t):
+    r'float'
+    return t
+
+def t_TYPE_DOUBLE(t):
+    r'double'
+    return t
+
+def t_TYPE_BOOL(t):
+    r'bool'
+    return t
+
+def t_TYPE_VOID(t):
+    r'void'
+    return t
+
+def t_TYPE_NULL(t):
+    r'null'
+    return t
+
+def t_ARROW(t):
+    r'->'
+    return t
+
+def t_FLOAT(t):
+    r'\d+\.\d+f'
+    t.value = float(t.value[:-1]) # elimina la 'f' del final para tomar el valor del decimal
+    return t
+
+def t_DOUBLE(t):
+    r'\d+\.\d+'
+    return t
+
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value) # Convertir el valor del token a un entero
+    return t
+
+def t_STRING(t):
+    r'\'[^\']*\''
+    t.value = t.value[1:-1] # Eliminar las comillas del principio y del final
+    return t
+
+def t_BOOL(t):
+    r'(true|false)'
+    t.value = bool(t.value)
+    return t
+
+def t_FUNCTION(t):
+    r'fun'
+    return t
+
+def t_RETURN(t):
+    r'return'
+    return t
+
+def t_IF(t):
+    r'if'
+    return t
+
+def t_ELSE(t):
+    r'else'
+    return t
+
+def t_FOR(t):
+    r'for'
+    return t
+
+def t_WHILE(t):
+    r'while'
+    return t
+
+def t_IDENTIFIER(t):
+    r'[a-zA-Z][a-zA-Z0-9_]*'
+    return t
+
+def t_COMMENT(t):
+    r'/\?.*'
+    pass
+
+def t_SPACE(t):
+    r'[^\S\n]'
+    pass
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# Se invoca la funcion cuando se da un caracter no reconocido
+def t_error(t):
+    global errores
+    estado = "** El token no es válido en la línea {:4} Valor `{:16}` Posición {:4} Len: {:5}".format(str(t.lineno), str(t.value), str(t.lexpos), str(len(t.value)))
+    errores.append(estado)
+    # Salta el token y sigue con el resto de la entrada
+    t.lexer.skip(1)
+
+analizador = lex.lex()
---
+++ Analizador_Sintactico/analizador_sintactico.py
@@ -0,0 +1,152 @@
+import ply.yacc as yacc
+from analizador_lexico import tokens, analizador
+
+# Definir las reglas de precedencia
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE', 'MODULO'),
+    ('left', 'POWER'),
+    ('left', 'COMPARISON', 'LESS_THAN', 'GREATER_THAN', 'GREATER_EQUAL', 'LESS_EQUAL', 'NOT_EQUAL'),
+    ('left', 'NOT'),
+    ('left', 'AND', 'OR'),
+)
+
+# Diccionario para almacenar los valores de las variables
+variables = {}
+
+# Definir las funciones correspondientes a cada regla de producción
+
+def p_F(p):
+    '''F : LEFT_PAREN E RIGHT_PAREN
+         | IDENTIFIER
+         | INT
+         | FLOAT
+         | DOUBLE
+         | STRING
+         | BOOL
+         | NOT E
+         | E COMPARISON E
+         | E LESS_THAN E
+         | E GREATER_THAN E
+         | E LESS_EQUAL E
+         | E GREATER_EQUAL E
+         | E NOT_EQUAL E
+         | E AND E
+         | E OR E'''
+    if len(p) == 2:  # IDENTIFIER, INT, FLOAT, DOUBLE, STRING, BOOL
+        if isinstance(p[1], str) and p[1] in variables:
+            p[0] = variables[p[1]]
+        else:
+            p[0] = p[1]
+    elif p[1] == '(':
+        p[0] = p[2]
+    elif p[1] == '!':
+        p[0] = not p[2]
+    elif len(p) == 4:
+        if p[2] == '==':
+            p[0] = p[1] == p[3]
+        elif p[2] == '!=':
+            p[0] = p[1] != p[3]
+        elif p[2] == '<':
+            p[0] = p[1] < p[3]
+        elif p[2] == '>':
+            p[0] = p[1] > p[3]
+        elif p[2] == '<=':
+            p[0] = p[1] <= p[3]
+        elif p[2] == '>=':
+            p[0] = p[1] >= p[3]
+        elif p[2] == '&&':
+            p[0] = p[1] and p[3]
+        elif p[2] == '||':
+            p[0] = p[1] or p[3]
+
+def p_E(p):
+    '''E : F'''
+
+def p_expression_plus(p):
+    'E : E PLUS E'
+    p[0] = p[1] + p[3]
+
+def p_expression_minus(p):
+    'E : E MINUS E'
+    p[0] = p[1] - p[3]
+
+def p_expression_times(p):
+    'E : E TIMES E'
+    p[0] = p[1] * p[3]
+
+def p_expression_divide(p):
+    'E : E DIVIDE E'
+    p[0] = p[1] / p[3]
+
+def p_expression_modulo(p):
+    'E : E MODULO E'
+    p[0] = p[1] % p[3]
+
+def p_expression_power(p):
+    'E : E POWER E'
+    p[0] = p[1] ** p[3]
+  
+def p_VD(p):
+    'VD : IDENTIFIER COLON TD ASSIGN_OP E'
+    variable_name = p[1]
+    data_type = p[3]
+    value = p[5]
+    variables[variable_name] = value
+
+def p_TD(p):
+    '''TD : TYPE_BOOL 
+          | TYPE_DOUBLE
+          | TYPE_FLOAT
+          | TYPE_INTEGER
+          | TYPE_NULL
+          | TYPE_STRING
+          | TYPE_VOID'''
+    p[0] = p[1]
+
+def p_FD(p):
+    '''FD : FUNCTION IDENTIFIER LEFT_PAREN PL RIGHT_PAREN ARROW TD B'''
+
+def p_S(p):
+    '''S : E SEMICOLON
+         | IfS
+         | WS
+         | FS
+         | RS
+         | VD SEMICOLON'''
+
+def p_B(p):
+    'B : LEFT_BRACKET SL RIGHT_BRACKET'
+
+def p_SL(p):
+    '''SL : S
+          | SL S''' 
+
+def p_PL(p):
+    '''PL : PL COMMA IDENTIFIER COLON TD
+          | IDENTIFIER COLON TD'''       
+
+def p_IfS(p):
+    '''IfS : IF LEFT_PAREN E RIGHT_PAREN B
+           | IF LEFT_PAREN E RIGHT_PAREN B ELSE B'''
+
+def p_WS(p):
+    'WS : WHILE LEFT_PAREN E RIGHT_PAREN B'
+
+def p_FS(p):
+    'FS : FOR IDENTIFIER IN IDENTIFIER LEFT_BRACKET SL RIGHT_BRACKET'
+
+def p_RS(p):
+    'RS : RETURN E SEMICOLON'
+
+def p_error(p):
+    if p:
+        print("Error sintáctico en línea {}, posición {}")
+
+parser = yacc.yacc()
+
+# Ahora puedes utilizar el parser para analizar tu entrada
+entrada = """x: int = 10;"""
+resultado = parser.parse(entrada, lexer=analizador)
+print(resultado)
+print(variables)
